22cc4550143d814e86aa28b121e48752bd73911b
Atef Ben Ali <atef@novatis.tn>
2017-08-02 10:28:19 +0100

    add 'api' route to 'index.js' file

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get('/test', function(req, res) {
    res.send('Test route!');
 });
 
+app.post('/api', function(req, res) {
+   res.send('API');
+});
+
 app.listen(3300, function () {
     console.log('The App is listening on http://localhost:3300.');
 });
